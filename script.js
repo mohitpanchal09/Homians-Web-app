2f854f6c9eb37bb3f888f18f3c9bd17425df98e4
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2022-05-08 01:20:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,84 @@
 document.addEventListener("DOMContentLoaded", () => {
-    function counter(id, start, end, duration) {
-     let obj = document.getElementById(id),
-      current = start,
-      range = end - start,
-      increment = end > start ? 1 : -1,
-      step = Math.abs(Math.floor(duration / range)),
-      timer = setInterval(() => {
-       current += increment;
-       obj.textContent = current;
-       if (current == end) {
-        clearInterval(timer);
-       }
-      }, step);
-    }
-    counter("count1", 0, 20, 4000);
-    counter("count2", 0, 200, 8000);
-    
-   });
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += increment;
+     obj.textContent = current;
+     if (current == end) {
+      clearInterval(timer);
+     }
+    }, step);
+  }
+  counter("count1", 0, 20, 4000);
+  counter("count2", 0, 200, 8000);
+  
+ });
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+let parent = this.parentNode;
+parent.classList.add("focus");
+}
+
+function blurFunc() {
+let parent = this.parentNode;
+if (this.value == "") {
+  parent.classList.remove("focus");
+}
+}
+
+inputs.forEach((input) => {
+input.addEventListener("focus", focusFunc);
+input.addEventListener("blur", blurFunc);
+});
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function () {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+      /* Toggle between hiding and showing the active panel */
+      var body = this.nextElementSibling;
+      if (body.style.display === "block") {
+          body.style.display = "none";
+      } else {
+          body.style.display = "block";
+      }
+  });
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
